6b05137f08d63a7315a9bc129d39d3ae7b9474f6
demagoit <33demagoit@gmail.com>
2024-05-16 21:06:09 +0300

    create comments schema

---
+++ app/src/repository/comments.py
@@ -0,0 +1,17 @@
+
+
+async def create_comment():
+    pass
+
+async def edit_comment():
+    pass
+
+async def get_comments_by_user_id():
+    pass
+
+async def get_comments_by_photo_id():
+    pass
+
+async def delete_comment():
+    pass
+
