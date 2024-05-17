efdec4cea7652e0426f7731ce4f8da86ab7c3fc1
demagoit <33demagoit@gmail.com>
2024-05-17 15:12:55 +0300

    post comment, get comments by user_id, photo_id, user&photo ids

--- app/src/repository/comments.py
+++ app/src/repository/comments.py
@@ -10,18 +10,44 @@ async def create_comment(user: User, body: CommentNewSchema, db: Session) -> Com
     comment = Comment(**body.model_dump())
     comment.user_id = user.id
     db.add(comment)
-    await db.commit()
-    comment = await db.refresh(comment)
+    # await db.commit()
+    db.commit()
+    # comment = await db.refresh(comment)
+    db.refresh(comment)
     return comment
 
-async def edit_comment():
-    pass
+# TODO - see routes
+# async def edit_comment(user: User, record_id: int, body: CommentNewSchema, db: Session):
+#     stmt = select(Comment).filter_by(id=record_id, photo_id=body.photo_id)
+#     # result = await db.execute(stmt)
+#     result = db.execute(stmt)
+#     result = result.scalar_one_or_none()
+#     if result:
+#         result.text = body.text
+#         # await db.commit()
+#         # await db.refresh(result)
+#         db.commit()
+#         db.refresh(result)
+#     return result
 
-async def get_comments_by_user_id():
-    pass
 
-async def get_comments_by_photo_id():
-    pass
+async def get_comments_by_user_id(user_id: int, offset: int, limit: int, db: Session):
+    stmt = select(Comment).filter_by(user_id=user_id).offset(offset).limit(limit)
+    # result = await db.execute(stmt)
+    result = db.execute(stmt)
+    return result.scalars().all()
+
+async def get_comments_by_photo_id(photo_id: int, offset: int, limit: int, db: Session):
+    stmt = select(Comment).filter_by(photo_id=photo_id).offset(offset).limit(limit)
+    # result = await db.execute(stmt)
+    result = db.execute(stmt)
+    return result.scalars().all()
+
+async def get_comments_by_user_and_photo_ids(user_id: int, photo_id: int, offset: int, limit: int, db: Session):
+    stmt = select(Comment).filter_by(user_id=user_id, photo_id=photo_id).offset(offset).limit(limit)
+    # result = await db.execute(stmt)
+    result = db.execute(stmt)
+    return result.scalars().all()
 
 async def delete_comment():
     pass
