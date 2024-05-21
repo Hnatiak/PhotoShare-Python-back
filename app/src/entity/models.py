c0329d8059f327514f58978bea6c91025d144a3e
demagoit <33demagoit@gmail.com>
2024-05-21 22:20:31 +0300

    tests

---
+++ app/src/entity/models.py
@@ -0,0 +1,96 @@
+import io
+import uuid
+import enum
+from sqlalchemy.orm import relationship, Mapped, mapped_column, backref, declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import UUID, Column, Integer, LargeBinary, String, Date, Boolean, func
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy import Enum
+
+Base = declarative_base()
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+class AssetType(enum.Enum):
+    origin: str = 'origin'
+    avatar: str = 'avatar'
+    greyscale: str = 'greyscale'
+    delete_bg: str = 'delete_bg'
+    oil_paint: str = 'oil_paint'
+    sepia: str = 'sepia'
+    outline: str = 'outline'
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    phone = Column(String(13), unique=True, nullable=True, default=None)
+    birthday = Column(Date, nullable=True, default=None)
+    created_at = Column("created_at", DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    role = Column('role', Enum(Role), default=Role.user)
+    isLoggedIn = Column(Boolean, default=False)
+    confirmed = Column(Boolean, default=False)
+
+
+class BlacklistToken(Base):
+    __tablename__ = 'blacklist_tokens'
+    id = Column(Integer, primary_key=True)
+    token = Column(String(500), unique=True, nullable=False)
+    blacklisted_on = Column(DateTime, default=func.now())
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    photo_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("photos.id", ondelete="CASCADE"), nullable=False)
+    text: Mapped[str] = mapped_column(String(500), nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    user: Mapped["User"] = relationship("User", backref="comments", lazy="joined")
+    photo: Mapped["Photo"] = relationship("Photo", backref=backref("comments", cascade="all, delete"), lazy="joined")
+
+# table for photo and tag relationship
+class PhotoTag(Base):
+    __tablename__ = "phototags"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    photo_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("photos.id", ondelete="CASCADE"), nullable=False)
+    tag_id: Mapped[int] = mapped_column(ForeignKey("tags.id"), nullable=False)
+
+
+class Photo(Base):
+    __tablename__ = "photos"
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    # original_id: Mapped[uuid.UUID] = mapped_column(UUID, default=None)
+    asset_type = Column(ENUM(AssetType), default='origin', nullable=True)
+    created_at = Column("created_at", DateTime, default=func.now(), index=True)
+    updated_at = Column("updated_at", DateTime, default=func.now())
+    user_id = Column("user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None)
+    url: Mapped[str] = mapped_column(String(2048), nullable=False)    
+    description: Mapped[str] = mapped_column(String(2200), nullable=True, index=True)
+    tags: Mapped[list["Tag"]] = relationship(secondary='phototags', back_populates='photos', lazy="joined")
+    user = relationship("User", backref="photos")
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at = Column("created_at", DateTime, default=func.now())
+    name: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    photos: Mapped[list["Photo"]] = relationship(secondary='phototags', back_populates='tags', lazy="joined")
+
+class QRCode(Base):
+    __tablename__ = "qr_codes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    photo_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("photos.id", ondelete="CASCADE"), nullable=False)
+    qr_code: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
---
+++ app/src/schemas/schemas.py
@@ -0,0 +1,162 @@
+import uuid
+import enum
+from typing import Dict, Hashable, List, Optional, Annotated, TypeVar
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr, PastDate, PlainSerializer, Strict, conset, UUID4
+# from pydantic_extra_types.phone_numbers import PhoneNumber
+from src.entity.models import Role, AssetType
+from datetime import date
+
+
+class UserSchema(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(min_length=5, max_length=50)
+
+# class UserModel(BaseModel):
+#     username: str = Field(min_length=2, max_length=16)
+#     email: str
+#     phone: str = Field(min_length=10, max_length=13)
+#     birthday: date
+#     password: str = Field(min_length=4, max_length=10)
+
+
+class UserUpdateSchema(BaseModel):
+    username: str = Field(min_length=3, max_length=40)
+    phone: str = Field(min_length=10, max_length=13)
+    birthday: date
+
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class RoleUpdateSchema(BaseModel):
+    role: Role
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: str
+    phone: str | None
+    birthday: date | None
+    created_at: datetime
+    avatar: str
+    role: Role
+
+    # class Config:
+    #     from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+# class UserDb(BaseModel):
+#     id: int
+#     username: str
+#     email: str
+#     created_at: datetime
+#     avatar: str
+#     role: Role
+#
+#     class Config:
+#         from_attributes = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class SearchUserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    phone: str | None
+    birthday: date | None
+    created_at: datetime
+    avatar: str
+    role: Role
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserResponseAll(BaseModel):
+    user: UserDb
+
+    # class Config:
+    #     from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
+
+
+class CommentResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    photo_id: Annotated[UUID4, Strict(False)]
+    text: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class CommentNewSchema(BaseModel):
+    photo_id: Annotated[UUID4, Strict(False)]
+    text: str
+
+
+class PhotoBase(BaseModel):   
+    url: str
+    description: Optional[str] = Field(None, max_length=2200)    
+    tags: Optional[conset(str, max_length=5)] # type: ignore
+    asset_type: AssetType = AssetType.origin
+
+
+class LinkType(enum.Enum):
+    url: str = "URL"
+    qr_code: str = "QR Code"
+
+class TagBase(BaseModel):
+    name: str
+
+    # class Config:
+    #     from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+# tags output format is controlled here
+
+def tags_serializer(tags: TagBase) -> str:
+    names = [f'#{tag.name}' for tag in tags]
+    return " ".join(names)    
+
+
+CustomStr = Annotated[List[TagBase], PlainSerializer(tags_serializer, return_type=str)]
+UUIDString = Annotated[UUID4, PlainSerializer(lambda x: str(x), return_type=str)]
+
+
+class SimpleComment(BaseModel):
+    user_id: int
+    text: str
+
+
+class PhotoResponse(PhotoBase):
+    id: Annotated[UUID4, Strict(False)]
+    created_at: datetime
+    updated_at: datetime
+    url: str
+    # tags: list[TagBase]
+    tags: CustomStr
+    comments: list[SimpleComment]
+
+    # class Config:
+    #     from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+
+class PhotoTransform(BaseModel):
+    ...
