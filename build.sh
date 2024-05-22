c937b26da45cfd4d401d3a882d221309e1a34ee2
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-05-22 19:24:19 +1000

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install poetry
+poetry update
+
+alembic upgrade head
--- initialize.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-python3 -m  pip install poetry
-python3 -m  poetry update
-python3 -m  alembic upgrade head
