351c70798b7065e5abbefb50b0dec693973b0662
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-05-23 22:37:55 +1000

    caching qrcode, return qrcode on save

--- app/src/repository/qrcode.py
+++ app/src/repository/qrcode.py
@@ -1,27 +1,34 @@
 import base64
 import uuid
 import io
-from typing import List
-from sqlalchemy import func, and_
-from sqlalchemy.orm import Session, joinedload
-from sqlalchemy.orm.relationships import _RelationshipDeclared
-from sqlalchemy.orm.properties import ColumnProperty
-from fastapi import UploadFile
-from src.entity.models import Photo, Tag, User, AssetType, QRCode
-from datetime import datetime, timedelta
-from src.schemas.schemas import PhotoBase, PhotoResponse
+from sqlalchemy.orm import Session, Query
+from src.entity.models import User, QRCode
+from src.services.cache import QueryExecutor, CacheableQueryExecutor
 
-async def save_qr_code(photo_id: uuid.UUID, qr_code_binary: io.BytesIO, user: User, db: Session) -> None:
-    qr_code = QRCode(photo_id=photo_id, qr_code=base64.b64encode(qr_code_binary.getvalue()))
-    db.add(qr_code)
-    db.commit()
-    db.refresh(qr_code)    
-    return bool(qr_code)
+class QRCodeRepository:
+    def __init__(self, query_executor: QueryExecutor = None) -> None:
+        self.query_executor = query_executor
+    
+    async def __first(self, id_key, query:Query):
+        if self.query_executor:
+            return await self.query_executor.get_first(id_key=id_key, query=query)
+        return query.first()
 
-async def read_qr_code(photo_id: uuid.UUID, user: User, db: Session) -> io.BytesIO | None:
-    qr_code = db.query(QRCode).filter(QRCode.photo_id == photo_id).first()    
-    qr_stream = None 
-    if qr_code:
-        qr_stream = io.BytesIO(base64.b64decode(qr_code.qr_code))
-        qr_stream.seek(0)
-    return qr_stream
+    async def save_qrcode(self, photo_id: uuid.UUID, qr_code_binary: io.BytesIO, user: User, db: Session) -> QRCode:
+        qr_code = QRCode(photo_id=photo_id, qr_code=base64.b64encode(qr_code_binary.getvalue()))
+        db.add(qr_code)
+        db.commit()
+        db.refresh(qr_code)    
+        return qr_code
+
+    async def read_qrcode(self, photo_id: uuid.UUID, user: User, db: Session) -> io.BytesIO | None:
+        query = db.query(QRCode).filter(QRCode.photo_id == photo_id)
+        qr_code = await self.__first(id_key=photo_id, query=query)    
+        qr_stream = None 
+        if qr_code:
+            qr_stream = io.BytesIO(base64.b64decode(qr_code.qr_code))
+            qr_stream.seek(0)
+        return qr_stream
+
+
+repository_qrcode = QRCodeRepository(query_executor=CacheableQueryExecutor())
