aa14b5877cdf21e888fa28b10aa4b06b0e1b3fe5
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-05-23 20:31:23 +1000

    photo repo tests updated

--- app/tests/test_repository_photos.py
+++ app/tests/test_repository_photos.py
@@ -12,7 +12,6 @@ from sqlalchemy import func, and_, or_
 from sqlalchemy.orm import Session, joinedload, Query
 from src.entity.models import Photo, Tag, User, AssetType
 from src.schemas.schemas import PhotoBase, PhotoUpdate
-from src.repository.exceptions import AccessDeniedException
 from src.repository.photos import PhotosRepository
 from src.services.cache import CacheableQueryExecutor, QueryExecutor
 
@@ -20,10 +19,13 @@ from src.services.cache import CacheableQueryExecutor, QueryExecutor
 class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
-        self.mock_session = Mock(spec=Session)
-        self.mock_query = Mock(spec=Query)
-        self.mock_photo = Mock(spec=Photo)
-        self.mock_tag = Mock(spec=Tag)
+        self.mock_session = MagicMock(spec=Session)
+        self.mock_session.delete.return_value = None
+        self.mock_session.add.return_value = None
+        self.mock_session.commit.return_value = None
+        self.mock_query = MagicMock(spec=Query)
+        self.mock_photo = MagicMock(spec=Photo, id=uuid4(), tags=list[Tag]())
+        self.mock_tag = MagicMock(spec=Tag)
         self.user = User(id=1)
         self.repository = PhotosRepository(query_executor=AsyncMock(spec=CacheableQueryExecutor))
         self.repository._PhotosRepository__ensure_tags = AsyncMock()
@@ -74,7 +76,7 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
 
         photo = await self.repository.get_photo(photo_id, self.user, self.mock_session)
 
-        self.mock_session.query.assert_called_once()
+        self.mock_session.query.assert_called_once()        
         self.repository.query_executor.get_first.assert_called_once()
         self.assertEqual(photo, self.mock_photo)
 
@@ -91,9 +93,7 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
     async def test_create_photo(self):
         tags = ["nature", "life"]
         tag_models = [Tag(id=1, name="nature"), Tag(id=2, name="life")]
-        body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=tags)
-        self.mock_session.add.return_value = None
-        self.mock_session.commit.return_value = None    
+        body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=tags)          
         self.repository._PhotosRepository__ensure_tags.return_value=tag_models
 
         photo = await self.repository.create_photo(body, self.user, self.mock_session)
@@ -112,8 +112,6 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
     async def test_create_photo_with_existing_tags(self):
         existing_tag = Tag(id=1, name="nature")
         body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=["nature"])
-        self.mock_session.add.return_value = None
-        self.mock_session.commit.return_value = None
         self.repository._PhotosRepository__ensure_tags.return_value=[existing_tag]
         
         photo = await self.repository.create_photo(body, self.user, self.mock_session)
@@ -130,68 +128,90 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
             body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=["nature", "beach", "landscape", "sky", "cloud", "exceed"])
             await self.repository.create_photo(body, self.user, self.mock_session)
 
-    async def test_update_photo_details(self):
-        photo_id = uuid4()
-        tags = ["new_tag"]
-        body = PhotoUpdate(description="updated description", tags=tags)
-        self.mock_session.query.return_value = Mock(filter=Mock(return_value=Mock(first=self.mock_photo)))
-        self.mock_photo.tags = set()
-        self.mock_ensure_tags = AsyncMock(return_value=[Mock(spec=Tag, name="new_tag")])
-        self.mock_session.add.return_value = None
-        self.mock_session.commit.return_value = None
+    async def test_update_photo_details(self):        
+        tag1 = Tag(name="tag1")
+        tag2 = Tag(name="tag2")
+        tag3 = Tag(name="tag3")
+        tag4 = Tag(name="tag4")
+        existing_tag_models = [tag1, tag2]
+        new_tags = [tag3.name, tag4.name]
+        new_tag_models = [tag3, tag4]
+        body = PhotoUpdate(description="updated description", tags=new_tags)
+        self.repository._PhotosRepository__ensure_tags.return_value = new_tag_models
+        self.mock_photo.tags = existing_tag_models[:]
+        
+        await self.repository.update_photo_details(self.mock_photo, body, self.user, self.mock_session)
 
-        await self.repository.update_photo_details(photo_id, body, self.user, self.mock_session, False)
-
-        self.mock_session.query.assert_called_once()
-        self.repository._PhotosRepository__ensure_tags.assert_called_once_with(tags=tags, db=self.mock_session)
-        self.mock_photo.description = body.description
-        self.mock_photo.tags.add(self.mock_ensure_tags.return_value[0])
+        self.repository._PhotosRepository__ensure_tags.assert_called_once_with(tags=list(set(new_tags)), db=self.mock_session)
         self.mock_session.add.assert_called_once()
-        self.mock_session.commit.has_calls(call(), call())
+        self.mock_session.commit.assert_has_calls([call(), call()])
         self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(photo_id)
+        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(self.mock_photo.id)
+        self.assertEqual(self.mock_photo.description, body.description)        
+        self.assertListEqual(self.mock_photo.tags, existing_tag_models + new_tag_models)
 
+    async def test_update_photo_details_too_many_tags(self):        
+        tag1 = Tag(name="tag1")
+        tag2 = Tag(name="tag2")
+        tag3 = Tag(name="tag3")
+        tag4 = Tag(name="tag4")
+        tag5 = Tag(name="tag5")
+        tag6 = Tag(name="tag6")
+        existing_tag_models = [tag1, tag2, tag3, tag4]
+        new_tags = [tag5.name, tag6.name]
+        new_tag_models = [tag5, tag6]
+        body = PhotoUpdate(description="updated description", tags=new_tags)
+        self.repository._PhotosRepository__ensure_tags.return_value = new_tag_models
+        self.mock_photo.tags = existing_tag_models[:]
+        
+        with self.assertRaises(IndexError):
+            await self.repository.update_photo_details(self.mock_photo, body, self.user, self.mock_session)
 
-    async def test_update_photo_details_with_authorization_error(self):
-        photo_id = uuid4()
-        body = PhotoUpdate(description="updated description", tags=[])        
-        self.mock_photo.user = Mock(spec=User)
-        self.mock_photo.user.id = "different_user_id"
-        self.mock_session.query.return_value = Mock(filter=Mock(return_value=Mock(first=self.mock_photo)))
+        self.repository._PhotosRepository__ensure_tags.assert_not_called()
+        self.mock_session.add.assert_not_called()
+        self.mock_session.commit.assert_not_called()
+        self.repository.query_executor.invalidate_cache_for_all.assert_not_called()
+        self.repository.query_executor.invalidate_cache_for_first.assert_not_called()
+        self.assertNotEqual(self.mock_photo.description, body.description)        
+        self.assertNotEqual(self.mock_photo.tags, existing_tag_models + new_tag_models)
 
-        with self.assertRaises(AccessDeniedException):
-            await self.repository.update_photo_details(photo_id, body, self.user, self.mock_session, True)
+    async def test_update_photo_details_unique_tags(self):        
+        tag1 = Tag(name="tag1")
+        tag2 = Tag(name="tag2")
+        tag3 = Tag(name="tag3")        
+        uniquetag = Tag(name="uniquetag")
+        existing_tag_models = [tag1, tag2, tag3]
+        new_tags = [tag2.name, tag3.name, uniquetag.name]
+        body = PhotoUpdate(description="updated description", tags=new_tags)
+        self.repository._PhotosRepository__ensure_tags.return_value = [uniquetag]
+        self.mock_photo.tags = existing_tag_models[:]
+        
+        await self.repository.update_photo_details(self.mock_photo, body, self.user, self.mock_session)
+
+        self.repository._PhotosRepository__ensure_tags.assert_called_once_with(tags=[uniquetag.name], db=self.mock_session)
+        self.mock_session.add.assert_called_once()
+        self.mock_session.commit.assert_has_calls([call(), call()])
+        self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
+        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(self.mock_photo.id)
+        self.assertEqual(self.mock_photo.description, body.description)        
+        self.assertListEqual(self.mock_photo.tags, existing_tag_models + [uniquetag])
 
     async def test_remove_photo(self):
-        photo_id = uuid4()
-        self.mock_session.delete.return_value = None
-        self.mock_session.commit.return_value = None
-        self.repository.query_executor.get_first.return_value = self.mock_photo
+        
+        photo = await self.repository.remove_photo(self.mock_photo, self.user, self.mock_session)
 
-        photo = await self.repository.remove_photo(photo_id, self.user, self.mock_session, False)
-
-        self.mock_session.query.assert_called_once()
         self.mock_session.delete.assert_called_once_with(self.mock_photo)
         self.mock_session.commit.assert_called_once()
         self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(photo_id)
+        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(photo.id)
         self.assertIsInstance(photo, Photo)
 
     async def test_remove_photo_not_found(self):
-        photo_id = uuid4()
-        self.repository.query_executor.get_first.return_value = None
 
-        photo = await self.repository.remove_photo(photo_id, self.user, self.mock_session, False)
+        photo = await self.repository.remove_photo(None, self.user, self.mock_session)
 
-        self.mock_session.query.assert_called_once()
+        self.mock_session.delete.assert_not_called()
+        self.mock_session.commit.assert_not_called()
+        self.repository.query_executor.invalidate_cache_for_all.assert_not_called()
+        self.repository.query_executor.invalidate_cache_for_first.assert_not_called()
         self.assertIsNone(photo)
-
-    async def test_remove_photo_with_authorization_error(self):
-        photo_id = uuid4()
-        self.mock_session.query.return_value = Mock(filter=Mock(return_value=Mock(first=self.mock_photo)))
-        self.mock_photo.user = Mock(spec=User)
-        self.mock_photo.user.id = "different_user_id"
-
-        with self.assertRaises(AccessDeniedException):
-            await self.repository.remove_photo(photo_id, self.user, self.mock_session, True)
-
