76089c3a51e54d660f6b88c158694c7af054eb86
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-05-24 21:31:36 +1000

    Photo repo tests updated

--- app/tests/test_repository_photos.py
+++ app/tests/test_repository_photos.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session, Query
 from src.entity.models import Photo, Tag, User, AssetType
 from src.schemas.schemas import PhotoBase, PhotoUpdate
 from src.repository.photos import PhotosRepository
-from src.services.cache import CacheableQueryExecutor
+from src.services.cache import CacheableQueryExecutor, CacheableQuery
 
 
 class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
@@ -84,7 +84,8 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
         self.repository.query_executor.get_first.assert_called_once()
         self.assertIsNone(photo)
 
-    async def test_create_photo(self):
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_create_photo(self, event_trigger):
         tags = ["nature", "life"]
         tag_models = [Tag(id=1, name="nature"), Tag(id=2, name="life")]
         body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=tags)          
@@ -95,15 +96,16 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
         self.mock_session.add.assert_called_once_with(photo)
         self.mock_session.commit.assert_called_once()
         self.mock_session.refresh.assert_called_once_with(photo)
+        event_trigger.assert_called_once_with(event_prefix="photo", event_name="created")
         self.assertIsInstance(photo, Photo)
         self.assertEqual(photo.description, body.description)
         self.assertEqual(photo.url, body.url)
         self.assertEqual(photo.asset_type, AssetType.origin)
         self.assertEqual(photo.user, self.user)
-        self.assertEqual(photo.tags, tag_models)
-        self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
+        self.assertEqual(photo.tags, tag_models)        
 
-    async def test_create_photo_with_existing_tags(self):
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_create_photo_with_existing_tags(self, event_trigger):
         existing_tag = Tag(id=1, name="nature")
         body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=["nature"])
         self.repository._PhotosRepository__ensure_tags.return_value=[existing_tag]
@@ -114,15 +116,39 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
         self.mock_session.add.assert_called_once_with(photo)
         self.mock_session.commit.assert_called_once()
         self.mock_session.refresh.assert_called_once_with(photo)
+        event_trigger.assert_called_once_with(event_prefix="photo", event_name="created")
         self.assertIsInstance(photo, Photo)
         self.assertEqual(photo.tags[0], existing_tag)
 
-    async def test_create_photo_too_many_tags(self):        
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_create_photo_too_many_tags(self, event_trigger):        
         with self.assertRaises(ValidationError):
             body = PhotoBase(description="test photo", url="https://example.com/test.jpg", tags=["nature", "beach", "landscape", "sky", "cloud", "exceed"])
             await self.repository.create_photo(body, self.user, self.mock_session)
+        event_trigger.assert_not_called()
 
-    async def test_update_photo_details(self):        
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_create_transformation(self, event_trigger):
+        tag_models = [Tag(id=1, name="nature"), Tag(id=2, name="life")]
+        description="test photo"
+        url="https://example.com/test.jpg"
+        asset_type = AssetType.avatar
+        
+        photo = await self.repository.create_transformation(url, description, tag_models, asset_type, self.user, self.mock_session)
+
+        self.mock_session.add.assert_called_once_with(photo)
+        self.mock_session.commit.assert_called_once()
+        self.mock_session.refresh.assert_called_once_with(photo)
+        event_trigger.assert_called_once_with(event_prefix="photo", event_name="created")
+        self.assertIsInstance(photo, Photo)
+        self.assertEqual(photo.description, description)
+        self.assertEqual(photo.url, url)
+        self.assertEqual(photo.asset_type, AssetType.avatar)
+        self.assertEqual(photo.user, self.user)
+        self.assertEqual(photo.tags, tag_models)   
+
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_update_photo_details(self, event_trigger):        
         tag1 = Tag(name="tag1")
         tag2 = Tag(name="tag2")
         tag3 = Tag(name="tag3")
@@ -138,13 +164,13 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
 
         self.repository._PhotosRepository__ensure_tags.assert_called_once_with(tags=list(set(new_tags)), db=self.mock_session)
         self.mock_session.add.assert_called_once()
-        self.mock_session.commit.assert_has_calls([call(), call()])
-        self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(self.mock_photo.id)
+        self.mock_session.commit.assert_called_once()
+        event_trigger.assert_called_once_with(self.mock_photo.id, event_prefix="photo", event_name="updated")
         self.assertEqual(self.mock_photo.description, body.description)        
         self.assertListEqual(self.mock_photo.tags, existing_tag_models + new_tag_models)
 
-    async def test_update_photo_details_too_many_tags(self):        
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_update_photo_details_too_many_tags(self,event_trigger):        
         tag1 = Tag(name="tag1")
         tag2 = Tag(name="tag2")
         tag3 = Tag(name="tag3")
@@ -164,12 +190,12 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
         self.repository._PhotosRepository__ensure_tags.assert_not_called()
         self.mock_session.add.assert_not_called()
         self.mock_session.commit.assert_not_called()
-        self.repository.query_executor.invalidate_cache_for_all.assert_not_called()
-        self.repository.query_executor.invalidate_cache_for_first.assert_not_called()
+        event_trigger.assert_not_called()
         self.assertNotEqual(self.mock_photo.description, body.description)        
         self.assertNotEqual(self.mock_photo.tags, existing_tag_models + new_tag_models)
 
-    async def test_update_photo_details_unique_tags(self):        
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_update_photo_details_unique_tags(self, event_trigger):        
         tag1 = Tag(name="tag1")
         tag2 = Tag(name="tag2")
         tag3 = Tag(name="tag3")        
@@ -184,28 +210,27 @@ class TestAsyncPhotosRepository(unittest.IsolatedAsyncioTestCase):
 
         self.repository._PhotosRepository__ensure_tags.assert_called_once_with(tags=[uniquetag.name], db=self.mock_session)
         self.mock_session.add.assert_called_once()
-        self.mock_session.commit.assert_has_calls([call(), call()])
-        self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(self.mock_photo.id)
+        self.mock_session.commit.assert_called_once()
+        event_trigger.assert_called_once_with(self.mock_photo.id, event_prefix="photo", event_name="updated")
         self.assertEqual(self.mock_photo.description, body.description)        
         self.assertListEqual(self.mock_photo.tags, existing_tag_models + [uniquetag])
 
-    async def test_remove_photo(self):
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_remove_photo(self, event_trigger):
         
         photo = await self.repository.remove_photo(self.mock_photo, self.user, self.mock_session)
 
         self.mock_session.delete.assert_called_once_with(self.mock_photo)
         self.mock_session.commit.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_all.assert_called_once()
-        self.repository.query_executor.invalidate_cache_for_first.assert_called_once_with(photo.id)
+        event_trigger.assert_called_once_with(self.mock_photo.id, event_prefix="photo", event_name="deleted")
         self.assertIsInstance(photo, Photo)
 
-    async def test_remove_photo_not_found(self):
+    @patch("src.services.cache.CacheableQuery.trigger")
+    async def test_remove_photo_not_found(self, event_trigger):
 
         photo = await self.repository.remove_photo(None, self.user, self.mock_session)
 
         self.mock_session.delete.assert_not_called()
         self.mock_session.commit.assert_not_called()
-        self.repository.query_executor.invalidate_cache_for_all.assert_not_called()
-        self.repository.query_executor.invalidate_cache_for_first.assert_not_called()
+        event_trigger.assert_not_called()
         self.assertIsNone(photo)
